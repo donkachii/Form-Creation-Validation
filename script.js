041d5626533d3ab5b7ab0bea241b6793c768e827
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-07-22 23:17:57 +0100

    Added javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+
+  const username = document.getElementById("username").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let isValid = true;
+    let messages = [];
+
+    if (username.length < 3) {
+      isValid == false;
+      messages.push("Your Username mush be exactly or more than 3 characters");
+    } else if (!email.includes("@") && !email.includes(".")) {
+      isValid == false;
+      messages.push("Your email is incorrectly formatted.");
+    } else if (password.length < 8) {
+      isValid == false;
+      messages.push("Your password mush be exactly or more than 8 characters");
+    }
+
+    feedbackDiv.style.display = "block";
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+    } else {
+      feedbackDiv.textContent = messages.join("\n");
+    }
+  });
+});
