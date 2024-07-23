f766c7f6dc17c9934101c428791e58e03126e85d
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-07-23 07:19:07 +0100

    Added fetch data page

---
+++ fetch-data.js
@@ -0,0 +1,8 @@
+async function fetchUserData() {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  const dataContainer = document.getElementById("api-data");
+
+  //   try {
+  //     await fetch()
+  //   }
+}
