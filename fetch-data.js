19affbe9dc5db67579a6364b3a4f357d203e8813
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-07-23 07:42:31 +0100

    Added fetch User Data to html

--- fetch-data.js
+++ fetch-data.js
@@ -1,17 +1,33 @@
-async function fetchUserData() {
-  const apiUrl = "https://jsonplaceholder.typicode.com/users";
-  const dataContainer = document.getElementById("api-data");
+document.addEventListener("DOMContentLoaded", () => {
+  async function fetchUserData() {
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
+    const dataContainer = document.getElementById("api-data");
+    const userList = document.createElement("ul");
 
-  try {
-    const response = await fetch(apiUrl);
+    try {
+      const response = await fetch(apiUrl);
 
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const users = await response.json();
+      console.log(users);
+
+      dataContainer.innerHTML = "";
+
+      users.forEach(function (user) {
+        const list = document.createElement("li");
+        list.textContent = user.name;
+        userList.appendChild(list);
+      });
+
+      dataContainer.appendChild(userList);
+    } catch (e) {
+      dataContainer.innerHTML = "Failed to load user data.";
+      console.error("Error: ", e);
     }
-
-    const users = await response.json();
-    console.log(users);
-  } catch (e) {
-    console.error("Error: ", e);
   }
-}
+
+  fetchUserData();
+});
