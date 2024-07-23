dbcd3418078491f2d13bfd05508eca0f7ffcfcf3
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-07-23 07:23:10 +0100

    Added fetch User Data Function

--- fetch-data.js
+++ fetch-data.js
@@ -2,7 +2,16 @@ async function fetchUserData() {
   const apiUrl = "https://jsonplaceholder.typicode.com/users";
   const dataContainer = document.getElementById("api-data");
 
-  //   try {
-  //     await fetch()
-  //   }
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const users = await response.json();
+    console.log(users);
+  } catch (e) {
+    console.error("Error: ", e);
+  }
 }
